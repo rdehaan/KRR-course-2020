bc1090135591b7093c6600cdc624ede0b9ed0f56
Ronald de Haan <me@ronalddehaan.eu>
2020-04-24 15:19:30 +0200

    Add code for hw1

---
+++ hw1/sudokucore.py
@@ -0,0 +1,29 @@
+###
+### Solver that uses SAT encoding
+###
+def solve_sudoku_SAT(sudoku,k):
+    return None;
+
+###
+### Solver that uses CSP encoding
+###
+def solve_sudoku_CSP(sudoku,k):
+    return None;
+
+###
+### Solver that uses ASP encoding
+###
+def solve_sudoku_ASP(sudoku,k):
+    return None;
+
+###
+### Solver that uses ILP encoding
+###
+def solve_sudoku_ILP(sudoku,k):
+    return None;
+
+###
+### Propagation function to be used in the recursive sudoku solver
+###
+def propagate(sudoku_possible_values,k):
+    return sudoku_possible_values;
