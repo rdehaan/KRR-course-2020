f3e17f95fd8256fb3c954180e742e63aeccd7f3b
Philip Goto <philip.goto@gmail.com>
2020-06-04 14:32:45 +0200

    Remove semicolons

--- hw1/sudoku_core.py
+++ hw1/sudoku_core.py
@@ -2,28 +2,28 @@
 ### Propagation function to be used in the recursive sudoku solver
 ###
 def propagate(sudoku_possible_values,k):
-    return sudoku_possible_values;
+    return sudoku_possible_values
 
 ###
 ### Solver that uses SAT encoding
 ###
 def solve_sudoku_SAT(sudoku,k):
-    return None;
+    return None
 
 ###
 ### Solver that uses CSP encoding
 ###
 def solve_sudoku_CSP(sudoku,k):
-    return None;
+    return None
 
 ###
 ### Solver that uses ASP encoding
 ###
 def solve_sudoku_ASP(sudoku,k):
-    return None;
+    return None
 
 ###
 ### Solver that uses ILP encoding
 ###
 def solve_sudoku_ILP(sudoku,k):
-    return None;
+    return None
