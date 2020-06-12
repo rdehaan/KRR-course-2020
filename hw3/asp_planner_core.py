52bf21ea5a8022aa4a48df522328b6752a28979d
Ronald de Haan <me@ronalddehaan.eu>
2020-06-12 09:42:30 +0200

    Add comments to hw3

--- hw3/asp_planner_core.py
+++ hw3/asp_planner_core.py
@@ -3,8 +3,21 @@ import planning
 
 import clingo
 
-###
-###
-###
 def solve_planning_problem_using_ASP(planning_problem,t_max):
+    """
+    If there is a plan of length at most t_max that achieves the goals of a given planning problem,
+    starting from the initial state in the planning problem, returns such a plan of minimal length.
+    If no such plan exists of length at most t_max, returns None.
+
+    NOTE: still needs to be implemented. Currently returns None for every input.
+
+    Parameters:
+        planning_problem (PlanningProblem): Planning problem for which a shortest plan is to be found.
+        t_max (int): The upper bound on the length of plans to consider.
+
+    Returns:
+        (list(Expr)): A list of expressions (each of which specifies a ground action) that composes
+        a shortest plan for planning_problem (if some plan of length at most t_max exists),
+        and None otherwise.
+    """
     return None;
